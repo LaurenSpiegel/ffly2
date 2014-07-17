d4d3686fb35eaf594688f6cf1629118303d35b28
Scott Maloff <scottmaloff@Scotts-MacBook-Pro.local>
2014-07-17 15:26:21 -0700

    about to add custom html

--- app/assets/javascripts/demo.js
+++
@@ -1,7 +0,0 @@
-
-
-	$('.colors').click(function(){
-		var title = jQuery(this).attr('title');		
-		jQuery('#templateColor').attr('href', 'css/colors/' + title + '.css');				
-	  	return false;
-    });	
